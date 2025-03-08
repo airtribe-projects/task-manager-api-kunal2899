5a4818c3810c834b7a3d1f8560293167a6806e75
Kunal Jain <kunal.jain@peoplegrove.com>
2025-03-09 04:05:01 +0530

    feat: registered tasks router

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+require('dotenv').config({ path: './.env' });
 const app = express();
-const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const tasksRouter = require('./routes/tasks/tasks.router');
 
-app.listen(port, (err) => {
+const PORT = process.env.PORT || 3000;
+
+// Routes
+app.use('/v1/tasks', tasksRouter);
+
+app.listen(PORT, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
     }
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${PORT}`);
 });
 
 
